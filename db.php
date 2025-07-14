e86e279cd189ab2b46325469338033dc96d645d2
Hiren Akhed <22bce014@nirmauni.ac.in>
2025-07-15 01:25:25 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,20 +1,8 @@
 <?php
-// $conn = mysqli_connect("localhost", "root", "", "dwar_db");
-
-// if (!$conn) {
-//   mysql.railway.internal  die("Connection failed: " . mysqli_connect_error());
-// }
-
-$conn = mysqli_connect(
-    "mysql://root:XBmbaFsqkgupVbZUKBgEYbzTXaYZqPdb@yamabiko.proxy.rlwy.net:58186/railway",       // Host
-    "root",                         // Username
-    "XBmbaFsqkgupVbZUKBgEYbzTXaYZqPdb", // Password
-    "railway",                      // Database name
-    3306                            // Port
-);
+$conn = mysqli_connect("localhost", "root", "", "dwar_db");
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
 ?>
+
